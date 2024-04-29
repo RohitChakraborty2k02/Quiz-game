d5643d7348f5507016b29b7f782c34eddb176d11
RohitChakraborty2k02 <rohitlevi143@gmail.com>
2024-04-29 13:08:14 +0530

    First Commit

---
+++ src/Rules.java
@@ -0,0 +1,69 @@
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Rohit's Quiz");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Times New Roman", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. All the questions are based on India's Qeography and History." + "<br><br>" +
+                "2. For each question an ample amount of time of 15 seconds is given." + "<br><br>" +
+                "3. Use of electronic devices is strictly prohibited." + "<br><br>" +
+                "4. As a super power 50-50 can be used for once." + "<br><br>" +
+                "5. Honestly give the Quiz" + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
